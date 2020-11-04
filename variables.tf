30f15b8850f458fe3366ec02837676ab1c79a7cd
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-11-04 18:20:07 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,9 @@
-variable "domain_name_2" {}
+#############################
+
+variable "aviatrix" {default = True}
 
 variable "domain_name_1" {}
+
+variable "domain_name_2" {}
 
 variable "ctrl_password" {}
