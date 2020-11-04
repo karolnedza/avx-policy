a8120d2cb493f556b1755acbe3f17f444d01cba6
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-11-04 18:45:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ provider "aviatrix" {
 ##### Aviatrix Security Domain Connection Policy
 
 resource "aviatrix_segmentation_security_domain_connection_policy" "security_domain_connection_policy" {
-  domain_name_1 = var.domain_name_1
-  domain_name_2 = var.domain_name_2
+  domain_name_1 = var.domain_name_2
+  domain_name_2 = var.domain_name_1
 }
