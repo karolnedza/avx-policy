677aa044ad84800417acea1b2bde14c439e54ada
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-11-04 11:31:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 provider "aviatrix" {
   username     = "admin"
   password      = var.ctrl_password
-  controller_ip = "35.171.31.227"
+  controller_ip = "18.157.190.93"
   version       = "2.17.0"
 }
 
